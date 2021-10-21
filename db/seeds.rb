2b77ebfc74961ebc685e94fbe783170a3a647701
yaroamaro219 <yaroamari@gmail.com>
2021-10-21 14:12:37 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Event.create([name:'Bricolage', location:'162 5th Ave Brooklyn NY', time: '7-9', price: '$75', attendees:'0'])
-Event.create([name:'Chela', location:'408 5th Ave, Brooklyn, NY 11215', time: '7-9', price: '$75', attendees:'0'])
-Event.create([name:'Bar Crudo', location:'412 5th Ave, Brooklyn, NY 11215', time: '7-9', price: '$75', attendees:'0'])
+
+Event.destroy_all
+
+Event.create([name:'Bricolage', location:'162 5th Ave Brooklyn NY', time: '7-9', price: '$75', attendees:'0', date:'11/18/2021'])
+Event.create([name:'Chela', location:'408 5th Ave, Brooklyn, NY 11215', time: '7-9', price: '$75', attendees:'0', date:'11/19/2021'])
+Event.create([name:'Bar Crudo', location:'412 5th Ave, Brooklyn, NY 11215', time: '7-9', price: '$75', attendees:'0', date:'11/21/2021'])
 puts "#{Event.count} events were created"
